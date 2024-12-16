41b607e956c16d62d65f481cf897b0c74aa774cb
김상겸 <44227066+vhf1030@users.noreply.github.com>
2024-12-16 19:06:04 +0900

    [level 2] Title: 짝지어 제거하기, Time: 78.58 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,14 +1,11 @@
 def solution(s):
-    list_left = list(s)
-    list_right = []
-    while list_left:
-        # print(list_left, list_right)
-        list_right.append(list_left.pop())
-        if len(list_right) < 2:
-            continue
-        if list_right[-1] == list_right[-2]:
-            list_right.pop()
-            list_right.pop()
-        if not list_left and not list_right:
-            return 1
-    return 0
+    remain = [""]
+    for c in s:
+        if remain[-1] == c:
+            remain.pop()
+        else:
+            remain.append(c)
+    if len(remain) == 1:
+        return 1
+    else:
+        return 0
