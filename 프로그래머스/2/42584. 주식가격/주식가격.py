123ce1e2f178b5f7e0688394d47c6d156412d63e
김상겸 <44227066+vhf1030@users.noreply.github.com>
2024-12-23 17:18:18 +0900

    [level 2] Title: 주식가격, Time: 33.70 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,18 +1,20 @@
 def solution(prices):
-    answer = [0]
-    stack = [(prices.pop(), 0)]  # 마지막 가격, 시간
-    prices.reverse()
-    for p in prices:  # for문이 좀더 빠름
-    # while prices:
-    #     p = prices.pop()
-        sec = 1
+    answer = [0]  # 마지막 정답은 항상 0
+    stack = []  # 떨어진 가격, 떨어지는데 걸린 시간
+    for p in prices[-2::-1]:  # prices[-2::-1] == prices[:-1][::-1]
+        t = 1
         while stack:
-            s = stack.pop()  # 마지막 가격이 더 높은 경우 저장하지 않음
-            if p > s[0]:
-                stack.append(s)  # 마지막 가격이 더 낮은 경우만 저장함
+            p_tmp, t_tmp = stack.pop()  # (가장 최근의) 떨어진 가격, 떨어지는데 걸린 시간
+            if p <= p_tmp:  # 가격이 떨어지지 않은 경우 - 기존의 떨어진 가격 및 시간 정보 제거
+                t += t_tmp  # 가격이 떨어지는데 걸린 시간 추가
+            else:  # 가격이 떨어진 경우
+                stack.append((p_tmp, t_tmp))  # 떨어진 가격 및 시간 정보 다시 저장
                 break
-            sec += s[1]  # 현재 가격에 시간 업데이트
-        stack.append((p, sec))  # 현재 가격 정보를 마지막 가격으로 옮김
-        answer.append(sec)
+            
+        stack.append((p, t))  # 현재 가격과 떨어지는데 걸린 시간 저장
+        # print(p, t, stack)
+        answer.append(t)
+        
     answer.reverse()
     return answer
+# 깃허브 제출용
