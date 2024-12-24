4d296ffbfa36693216d8de79bf85a184910d5d98
김상겸 <44227066+vhf1030@users.noreply.github.com>
2024-12-24 14:00:45 +0900

    [Silver V] Title: 방 번호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,10 +1,8 @@
 N = input()
-num_dict = dict()
-for _ in range(0, 10):
-    n = str(_)
-    num_dict[n] = N.count(n)
-    # print(n, ':', N.count(n))
-num_dict['6'] = int((num_dict['6'] + num_dict['9'] - 0.1) // 2) + 1
-num_dict['9'] = 0
+num_arr = [0] * 10
+for n in range(10):
+    num_arr[n] = N.count(str(n))
 
-print(max(num_dict.values()))
+num_arr[6] = int((num_arr[6] + num_arr[9] - 0.1) // 2 + 1)
+num_arr[9] = 0
+print(max(num_arr))
