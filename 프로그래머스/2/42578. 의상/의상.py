7ab439edd67b7bddbb95ca6aa9a6754f00920e75
김상겸 <44227066+vhf1030@users.noreply.github.com>
2025-01-02 13:50:17 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,12 @@
 def solution(clothes):
     clothes_count_dict = {}
     for _, c in clothes:
-        ccnt = clothes_count_dict.get(c)
-        if ccnt:
-            clothes_count_dict[c] += 1
-        else:
-            clothes_count_dict[c] = 1
+        clothes_count_dict[c] = clothes_count_dict.get(c, 0) + 1
+        # ccnt = clothes_count_dict.get(c)
+        # if ccnt:
+        #     clothes_count_dict[c] += 1
+        # else:
+        #     clothes_count_dict[c] = 1
     answer = 1
     for c in clothes_count_dict:
         answer *= (clothes_count_dict[c] + 1)
