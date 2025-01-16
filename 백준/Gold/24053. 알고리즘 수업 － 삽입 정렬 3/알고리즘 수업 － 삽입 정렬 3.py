15f6ef9e09daa83bf59d39a50d8c45839f0c0637
김상겸 <44227066+vhf1030@users.noreply.github.com>
2025-01-16 22:14:18 +0900

    [Gold V] Title: 알고리즘 수업 - 삽입 정렬 3, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/24053. 알고리즘 수업 － 삽입 정렬 3/알고리즘 수업 － 삽입 정렬 3.py
@@ -0,0 +1,29 @@
+N = int(input())
+arrA = list(map(int, input().split()))
+arrB = list(map(int, input().split()))
+
+def solution(N, arrA, arrB):
+    
+    for i in range(1, N):
+        if arrA == arrB:
+           return 1
+        
+        loc = i - 1
+        tmp = arrA[i]
+    
+        while 0 <= loc and tmp < arrA[loc]:
+            arrA[loc+1] = arrA[loc]
+            if arrA == arrB:
+                return 1
+            loc -= 1
+            
+        if loc + 1 != i:
+            arrA[loc + 1] = tmp
+            
+    if arrA == arrB:
+        return 1
+            
+    return 0
+
+print(solution(N, arrA, arrB))
+
