277de8de86b7b89e53bf50b6251300d7dfde556c
김상겸 <44227066+vhf1030@users.noreply.github.com>
2025-02-20 13:58:50 +0900

    [level 2] Title: 비밀 코드 해독, Time: 173.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+
+def solution(n, q, ans):
+    answer = 0
+    for c in combinations(range(1, n+1), 5):
+        answer += 1
+        a_set = set(c)
+        for q_tmp, a_tmp in zip(q, ans):
+            if len(a_set & set(q_tmp)) != a_tmp:
+                answer -= 1
+                break
+    return answer
